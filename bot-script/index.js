b73b12cec9a43b3929649b5cb6f8465a23326068
shivam.m <shivam.m@zohocorp.com>
2018-08-03 00:38:54 +0530

    Credentials local fix

--- bot-script/index.js
+++ bot-script/index.js
@@ -1,11 +1,20 @@
 let path = require("path");
 let logger = require(path.join(__dirname,"logger/logger.js"))
 let Bot = require(path.join(__dirname,"discord/bot.js"));
-let credentials = require(path.join(__dirname,"credentials/credentials.json"));
+let credentials;
+try
+{
+    credentials = require(path.join(__dirname,"credentials/credentials.json"));
+}
+catch(E)
+{
+    logger.error("No local credentials");
+}
 try
 {
     logger.log("Starting bot")
     let bot = new Bot(credentials.CLIENT_ID);
+    bot.addListeners();
     bot.run();
 }
 catch(E)
